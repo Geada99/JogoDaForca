93316b412c33615f4db2ebe21366bdf3f670a31a
Geada99 <mestrechess@gmail.com>
2024-06-06 17:20:36 +0100

    Images Upload

--- app/src/main/java/com/example/jogodaforca/MainActivity.kt
+++ app/src/main/java/com/example/jogodaforca/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.jogodaforca
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,6 +12,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.jogodaforca.ui.theme.JogoDaForcaTheme
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +32,7 @@ class MainActivity : ComponentActivity() {
             JogoDaForcaTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+
                 }
             }
         }
@@ -27,17 +40,15 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun GameScreen() {
+
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    JogoDaForcaTheme {
-        Greeting("Android")
-    }
+fun GameStatus(){
+
+}
+@Composable
+fun GameLayout(modifier: Modifier = Modifier) {
+
 }
