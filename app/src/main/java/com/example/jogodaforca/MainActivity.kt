58cf153c30e56a2dd06be31835c77be442108095
Geada99 <mestrechess@gmail.com>
2024-06-18 23:16:56 +0100

    Criação de Página Inicial  +  Criação e Implementação de navController

--- app/src/main/java/com/example/jogodaforca/MainActivity.kt
+++ app/src/main/java/com/example/jogodaforca/MainActivity.kt
@@ -1,38 +1,37 @@
 package com.example.jogodaforca
 
 import android.os.Bundle
-import android.widget.ImageView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.jogodaforca.ui.theme.JogoDaForcaTheme
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeDrawingPadding
-import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.jogodaforca.ui.theme.JogoDaForcaTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             JogoDaForcaTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-
+                    val navController = rememberNavController()
+                    NavHost(navController, startDestination = "home") {
+                        composable("home") { GameScreen(navController) }
+                        composable("game") { GameLayoutScreen() }
+                    }
                 }
             }
         }
@@ -40,15 +39,78 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GameScreen() {
+fun GameScreen(navController: NavController) {
+    val mediumPadding = dimensionResource(R.dimen.padding_medium)
 
+    Column(
+        modifier = Modifier
+            .statusBarsPadding()
+            .verticalScroll(rememberScrollState())
+            .safeDrawingPadding()
+            .padding(mediumPadding),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(R.string.app_name),
+            style = MaterialTheme.typography.titleLarge,
+            fontSize = 35.sp
+        )
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(mediumPadding),
+            verticalArrangement = Arrangement.spacedBy(mediumPadding),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = {
+                    navController.navigate("game")
+                }
+            ) {
+                Text(
+                    text = stringResource(R.string.play),
+                    fontSize = 16.sp
+                )
+            }
+        }
+    }
 }
 
 @Composable
-fun GameStatus(){
+fun GameLayoutScreen() {
+    val mediumPadding = dimensionResource(R.dimen.padding_medium)
 
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(mediumPadding),
+        verticalArrangement = Arrangement.spacedBy(mediumPadding),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        GameLayout()
+    }
 }
+
 @Composable
 fun GameLayout(modifier: Modifier = Modifier) {
+    val mediumPadding = dimensionResource(R.dimen.padding_medium)
 
+    Column(
+        verticalArrangement = Arrangement.spacedBy(mediumPadding),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.padding(mediumPadding)
+    ) {
+        Text("Game Layout")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GameScreenPreview() {
+    JogoDaForcaTheme {
+        val navController = rememberNavController()
+        GameScreen(navController)
+    }
 }
