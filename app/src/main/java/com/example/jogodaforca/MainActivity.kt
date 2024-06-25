e382502d5f9c223d840d6eff03e61fa122600494
Geada99 <mestrechess@gmail.com>
2024-06-25 20:33:50 +0100

    Já está bonitinho

--- app/src/main/java/com/example/jogodaforca/MainActivity.kt
+++ app/src/main/java/com/example/jogodaforca/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.jogodaforca
 
-import android.graphics.ColorSpace.Rgb
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
